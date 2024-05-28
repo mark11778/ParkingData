9e04d121645e2f246c7358a5152966c65eaaedab
Mark Ruzicka <marker11778@gmail.com>
2024-05-28 09:42:52 -0500

    google maps api actually working now

--- scr/website/backend/Public/app.js
+++ scr/website/backend/Public/app.js
@@ -12,3 +12,11 @@ window.onload = function() {
         })
         .catch(error => console.error('Error loading CSV data:', error));
 };
+
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+    });
+}
