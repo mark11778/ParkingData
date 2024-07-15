e2733594f340689b40a2f6cee415d9bdc47b19a8
Mark Ruzicka <marker11778@gmail.com>
2024-07-15 14:10:43 -0500

    switching

--- scr/website/backend/nonode/app.py
+++ scr/website/backend/nonode/app.py
@@ -9,23 +9,27 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-path = '../../../CollectedData' # use your path
 
-all_files = glob.glob(os.path.join(path , "*.csv"))
 
 li = []
 
-for filename in all_files:
-    df = pd.read_csv(filename, index_col=None, header=0)
-    li.append(df)
+# Function to read and concatenate CSV files
+def read_and_concatenate_files():
+    global ds 
+    path = '../../../CollectedData' # use your path
+    all_files = glob.glob(os.path.join(path , "*.csv"))
+    for filename in all_files:
+        df = pd.read_csv(filename, index_col=None, header=0)
+        li.append(df)
+    ds = pd.concat(li, axis=0, ignore_index=True)
+    ds.to_csv('test.csv', index=False)
 
-ds= pd.concat(li, axis=0, ignore_index=True)
+read_and_concatenate_files()
 
 
 @app.route('/')
 def index():
     global ds
-    print(ds)
     return render_template('index.html')
 
 @app.route('/python-function', methods=['POST'])
@@ -44,6 +48,7 @@ def python_function():
 def get_UserData(days, ticket_type):
     global ds
     df = ds
+    print(df)
 
     # days = request.args.get('days', default=30, type=int)
     # ticket_type = request.args.get('type', default="Parking", type=str)
@@ -57,9 +62,17 @@ def get_UserData(days, ticket_type):
     if (ticket_type is not None and "all" not in ticket_type):
         df = df[df['Type'].str.contains(ticket_type)]
 
-    df['Date Issue'] = pd.to_datetime(df['Date Issue'])
+    # df.loc[:, 'Date Issue'] = pd.to_datetime(df['Date Issue'])
+    for index in df.index:
+        try:
+            df.loc[index, 'Date Issue'] = pd.to_datetime(df.loc[index, 'Date Issue'])
+        except Exception as e:
+            print(f"Error converting row {df.loc[index]}: {e}")
+
 
     cutoff_date = datetime.now() - timedelta(days=days)
+
+    print(df)
 
     df = df[df['Date Issue'] >= cutoff_date]
     df = df[~df['Location'].str.contains("BUCKEYE LOT")]
@@ -84,11 +97,16 @@ def get_UserData(days, ticket_type):
 
     maxCount = grouped_by_type['Count'].max()
 
+    print(grouped_by_type)
+
     # max_count_per_location = grouped_by_type.groupby('Location')['Count'].max().reset_index(name='Max Count')
 
     # grouped_by_type = grouped_by_type.merge(grouped_by_type, on='Location', how='left')
 
-    # grouped_by_type['color'] = grouped_by_type.apply(lambda row: colors[round(row['Count'] / maxCount * 6)], axis=1)
+    grouped_by_type['color'] = grouped_by_type.apply(lambda row: colors[round(row['Count'] / maxCount * 6)], axis=1)
+    # for index in df.index:
+    #     grouped_by_type.loc[index, 'color'] = colors[round(grouped_by_type.loc[index, 'Count'] / maxCount * 6)]
+
 
 
 
